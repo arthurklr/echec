53882b0ffa091228ce5f8a92710a45c7a159877d
arthurklr_ <arthur.k2104@gmail.com>
2024-06-12 11:04:23 +0200

    Create bishop.php

---
+++ classes/bishop.php
@@ -0,0 +1,31 @@
+<?php
+class Bishop extends Piece {
+    public function getPossibleMoves($board) {
+        $moves = [];
+        $directions = [[-1, -1], [-1, 1], [1, -1], [1, 1]]; // Diagonals
+
+        foreach ($directions as $dir) {
+            for ($i = 1; $i <= 7; $i++) {
+                $x = $this->position[0] + $dir[0] * $i;
+                $y = $this->position[1] + $dir[1] * $i;
+
+                if ($x < 0 || $x > 7 || $y < 0 || $y > 7) {
+                    break; // Sortir du bord de l'échiquier
+                }
+
+                if ($board[$x][$y] === null) {
+                    $moves[] = [$x, $y];
+                } else {
+                    if ($board[$x][$y]->getColor() !== $this->color) {
+                        $moves[] = [$x, $y];
+                    }
+                    break; // Stopper après avoir rencontré une pièce
+                }
+            }
+        }
+
+        return $moves;
+    }
+}
+
+?>
